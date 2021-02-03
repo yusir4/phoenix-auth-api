70b416d8a75bf6631cadad04e676d527bb7f849f
Yusuf Demir <yusufdemir286634@gmail.com>
2021-02-03 18:29:52 +0300

    Rotalar düzenlendi

--- lib/app_web/router.ex
+++ lib/app_web/router.ex
@@ -6,10 +6,33 @@ defmodule MainModuleWeb.Router do
     plug :fetch_session
   end
 
+  pipeline :api_auth do
+    plug :ensure_authenticated
+  end
+
+  # Plug function
+  defp ensure_authenticated(conn, _opts) do
+    current_user_id = get_session(conn, :current_user_id)
+
+    if current_user_id do
+      conn
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> put_view(MainModuleWeb.ErrorView)
+      |> render("401.json", message: "Unauthenticated user")
+      |> halt()
+    end
+  end
+
   scope "/api", MainModuleWeb do
     pipe_through :api
-    resources "/users", UserController, except: [:new, :edit]
     post "/users/sign_in", UserController, :sign_in
+  end
+
+  scope "/api", MainModuleWeb do
+    pipe_through [:api, :api_auth]
+    resources "/users", UserController, except: [:new, :edit]
   end
 
   if Mix.env() in [:dev, :test] do
