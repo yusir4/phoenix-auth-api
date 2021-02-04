5a69e639ea047dc169751286a954980f0c54f0b4
Yusuf Demir <yusufdemir286634@gmail.com>
2021-02-04 16:37:24 +0300

    Joken paketi kuruldu

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule MainModule.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:bcrypt_elixir, "~> 2.3"}
+      {:bcrypt_elixir, "~> 2.3"},
       {:joken, "~> 2.3"}
     ]
   end
